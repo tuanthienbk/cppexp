7aecf4f3d41f9b5ede504884ae92bfa63ba4e5a8
Thien Nguyen <tuanthienbk@gmail.com>
2022-10-13 21:33:54 -0600

    remove generator stuff from source

--- source.h
+++ source.h
@@ -8,17 +8,18 @@ template <typename... Args>
 class source : public observable<Args...>
 {
     std::tuple<Args...> __value;
-    single_consumer_event new_value_event;
 public:
     explicit source(const std::tuple<Args...> &v) : __value(v) {}
     explicit source(const Args &...v) : __value(v...) {}
+
+    source(source&& other) : __value(std::move(other.__value)) {}
+    source& operator=(source&& other) { __value = std::move(other.__value);}
 
     template <size_t Index>
     void set(const std::remove_reference<decltype(std::get<Index>(__value))>::type &v)
     {
         std::get<Index>(__value) = v;
         this->notify_change(__value, std::index_sequence_for<Args...>{});
-        new_value_event.set();
     }
 
     template <size_t Index>
@@ -26,38 +27,16 @@ public:
     {
         std::get<Index>(__value) = v;
         this->notify_change(__value, std::index_sequence_for<Args...>{});
-        new_value_event.set();
     }
 
     void operator=(const std::remove_reference<decltype(std::get<0>(__value))>::type &v) requires (std::tuple_size_v<std::tuple<Args...>> == 1)
     {
         std::get<0>(__value) = v;
         this->notify_change(__value, std::index_sequence_for<Args...>{});
-        new_value_event.set();
     }
 
     template <size_t Index>
     decltype(std::get<Index>(__value)) get() { return std::get<Index>(__value); }
-
-    generator<std::tuple<Args...>> gen()
-    {
-        while (1)
-        {
-            co_yield __value;
-        }
-    }
-
-    template <typename F>
-    async<void> bind(F&& f)
-    {
-        auto g = this->gen();
-        for(auto&& v : g)
-        {
-            co_await new_value_event;
-            std::invoke(std::forward<F>(f), v);
-            new_value_event.reset();
-        }
-    }
 
 private:
     template <typename Tuple, size_t... Is>
